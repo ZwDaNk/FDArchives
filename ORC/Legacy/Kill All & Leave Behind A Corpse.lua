2e12028f77bdb2d13b07094452ac1cb261939dd2
LuaGunsX <134158550+LuaGunsX@users.noreply.github.com>
2025-02-25 20:11:34 -0500

    Update Kill All & Leave Behind A Corpse.lua

--- ORC/Legacy/Kill All & Leave Behind A Corpse.lua
+++ ORC/Legacy/Kill All & Leave Behind A Corpse.lua
@@ -1,4 +1,4 @@
--- Written by Java for older clients (<2014)
+-- Written by Java for older clients (2007 (?) - present)
 for _,plrs in pairs(game.Players:GetChildren()) do
 plrs.Character.Humanoid.Health = 0
 plrs.Character.Humanoid:Destroy()
